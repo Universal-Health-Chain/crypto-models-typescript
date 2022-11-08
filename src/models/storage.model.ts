e1a74e756d942677f6780b6b221e6d2002377020
Fernando Latorre <36719787+flatorre1@users.noreply.github.com>
2022-11-08 00:50:18 -0800

    fix export

--- src/models/storage.model.ts
+++ src/models/storage.model.ts
@@ -1,17 +1,19 @@
-/** Required "compositionStatus", "sectionCode" and "tags" (LOINC will be the default coding system if not specified).
- *  - tags (REQUIRED): non-personal data such as a list of FHIR resources, "SHC", "DGC", "COVID-19" tag, etc.
+/** Required "contentType", "compositionStatus" and "tags".
+ *  - contentType (REQUIRED): MIME type.
  *  - compositionStatus (REQUIRED): "preliminary", "amended", "final" (frontend should not use the "error" status).
- *  - sectionCode (REQUIRED): code for the health section or document category.
+ *  - tags (REQUIRED): non-personal data such as a list of FHIR resources, "SHC", "DGC", "COVID-19" tag, etc.
+ *  - sectionCode (Conditional): code for the health section or document category.
  *  - sectionSystem (Conditional): default is LOINC.
  *  - created (Conditional): required when creating a document, e.g.: "2019-03-23T06:35:22Z"
  *  - updated (Conditional): required when updating a document, e.g.: "2022-08-10T13:40:06Z"
+ *  - deactivated (Conditional): required when the storage object is disabled (before deleting).
  *  Note: the deactivation date is the "updated" timestamp.      
  */
  export interface MetaStorage {
     contentType: string; // required
     
     // From UHC
-    compositionStatus?: string; // "preliminary", "amended", "final" or "error".
+    compositionStatus:  string; // "preliminary", "amended", "final" or "error".
     sectionCode?:       string; // health section or document category.
     sectionSystem?:     string; // LOINC by default when it is not defined.
 
